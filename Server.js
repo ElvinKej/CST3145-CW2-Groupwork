19c47c66571f498bcf5cf95af070a8064244f4ab
ElvinKej <elvinkejiou27@gmail.com>
2023-01-10 23:37:44 +0000

    Express and cors

--- Server.js
+++ Server.js
@@ -1,3 +1,13 @@
 var express = require("express");
 const { allowedNodeEnvironmentFlags } = require("process");
 const cors = require("cors");
+
+var app = express();
+app.use(cors());
+// app.set.('json spaces', 0);
+app.set('json spaces', 1);
+
+app.use(function (req, res, next) {
+    console.log("incoming request" + req.url);
+    next();
+});
