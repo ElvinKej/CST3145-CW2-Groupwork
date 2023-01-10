08ac6ca5564dd42df651349c774f280197dfee63
ElvinKej <elvinkejiou27@gmail.com>
2023-01-10 23:38:13 +0000

    Get /user

--- Server.js
+++ Server.js
@@ -11,3 +11,18 @@ app.use(function (req, res, next) {
     console.log("incoming request" + req.url);
     next();
 });
+
+app.get("/", function(req, res) {
+    res.send("Welcome to our server");
+});
+
+app.get("/user", function(req, res) {
+    // res.send("The service has been called correctly");
+    let user =
+        {
+            "email": "user@gmail.com", 
+            "password" : "mypassword"
+        }
+    
+    res.json(user);
+});
